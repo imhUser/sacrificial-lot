54831795c4888fc71ad37e330be238541487b40d
imhUser <imhsacrificial@gmail.com>
2023-11-23 09:38:11 +0300

    Added footer layout

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        fontSize: "2vh",
+      }}
+    >
+      Copyright 2023 © Genç Teknik Tüm hakları saklıdır
+    </div>
+  );
+};
+
+export default Footer;
