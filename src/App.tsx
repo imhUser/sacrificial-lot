a2041ded9cca2e32b9e30e44302c88fcdc5259f8
imhUser <imhsacrificial@gmail.com>
2023-11-27 22:22:30 +0300

    Removed finalization page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Navbar from "./layouts/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Confirmation from "./pages/Confirmation";
-import Finalization from "./pages/Finalization";
 import ShareInfo from "./pages/ShareInfo";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/confirmation" element={<Confirmation/>}/>
-          <Route path="/finalization" element={<Finalization/>}/>
           <Route path="/shareInfo" element={<ShareInfo/>}/>
         </Routes>
         <Footer />
